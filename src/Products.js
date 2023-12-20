b9687b4df5b20433b2d9b3afa142e8635e70aab6
Sarah <Sarah.almarri.76@gmail.com>
2023-12-20 20:00:35 +0300

    done

--- src/Products.js
+++ src/Products.js
@@ -6,7 +6,7 @@ export const products = [
       "https://static.pullandbear.net/2/photos//2023/V/0/2/p/4597/722/711/4597722711_2_6_8.jpg?t=1677177259131&imwidth=1125",
   },
   {
-    name: "ESSENTIASLS Hoodie",
+    name: "Essentials Hoodie",
     price: "24kd",
     image:
       "https://leerdo.com/cdn/shop/products/January20211417_970x_28b0384e-d1fa-480a-8ecb-91a2d115a657.jpg?v=1660532761",
